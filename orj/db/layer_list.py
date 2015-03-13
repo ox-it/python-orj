3f0882757771659cecf60d9332e8b24558ac577f
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2015-03-13 11:25:21 +0000

    Support both versions of layer list.

--- orj/db/layer_list.py
+++ orj/db/layer_list.py
@@ -10,14 +10,26 @@ class LayerList(Group):
 
     def read(self, f, id_map):
         object_version = read_int(f)
-        if object_version > 1:
-            raise AssertionError
-        for i in range(read_int(f)):
-            object_type_name = read_string(f)
-            if object_type_name == 'DbLayer':
-                layer = Layer()
-                layer.read(f, id_map)
-                self.layers.append(layer)
+        if object_version > 2:
+            raise AssertionError(object_version)
+        if object_version >= 2:
+            j = read_int(f)
+            if j > 0:
+                b = read_buffer(f)
+                for i in range(j):
+                    object_buffer = read_buffer(b)
+                    name = read_string(object_buffer)
+                    if name == 'DbLayer':
+                        layer = Layer()
+                        layer.read(object_buffer, id_map)
+                        self.layers.append(layer)
+        elif object_version >= 1:
+            for i in range(read_int(f)):
+                object_type_name = read_string(f)
+                if object_type_name == 'DbLayer':
+                    layer = Layer()
+                    layer.read(f, id_map)
+                    self.layers.append(layer)
 
     def __iter__(self):
         return iter(self.layers)
